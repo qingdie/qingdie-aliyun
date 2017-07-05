70fe4eba6c117b0957d03908e7fa9541c170b291
qingdie <xueping@qingdie.net>
2017-07-05 21:22:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿module.exports = function (accessid, secret) {
+    var voiceurl = 'https://dyvmsapi.aliyuncs.com/';
+    var smsurl = 'https://dysmsapi.aliyuncs.com/';
+    var network = require('./lib/network');
+    network.app = {
+        AccessKeyId: accessid,
+        AccessKeySecret: secret
+    };
+    this.sendSms = function (msg) {
+        msg.Action = 'SendSms';
+        return network.post(smsurl, msg);
+    };
+    this.call = function (msg) {
+        msg.Action = 'SingleCallByVoice';
+        return network.post(voiceurl, msg);
+    };
+    return this;
+};
